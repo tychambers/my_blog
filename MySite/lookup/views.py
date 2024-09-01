99e37cf542d5004063a82489409c11bb7fba47ee
Tyler <Tylerm.chambers92@gmail.com>
2024-09-01 15:07:37 -0400

    small templates

--- MySite/lookup/views.py
+++ MySite/lookup/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, 'lookup/home.html', {})
+	return render(request, 'home.html', {})
 
 def about(request):
 	context = {
 	"variable1":[0,1,2,3,4,5,6],
 	"variable2": "Hello All!"
 	}
-	return render(request, 'lookup/about.html', context)
+	return render(request, 'about.html', context)
